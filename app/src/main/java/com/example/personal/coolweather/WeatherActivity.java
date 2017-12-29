f2a2de1d5e2b4a3c53cd221e0a18b9a10713d4f5
Tony <tong@gmail.com>
2017-12-29 09:42:36 +0800

    增加后台自动更新

--- app/src/main/java/com/example/personal/coolweather/WeatherActivity.java
+++ app/src/main/java/com/example/personal/coolweather/WeatherActivity.java
@@ -1,5 +1,6 @@
 package com.example.personal.coolweather;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.media.Image;
@@ -21,6 +22,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.example.personal.coolweather.Service.AutoUpdateService;
 import com.example.personal.coolweather.gson.Forecast;
 import com.example.personal.coolweather.gson.Weather;
 import com.example.personal.coolweather.utils.HttpUtils;
@@ -198,6 +200,8 @@ public class WeatherActivity extends AppCompatActivity {
             String airString = "空气指数: " + weather.suggestion.air.info;
             airTextView.setText(airString);
         }
+        Intent intent = new Intent(this, AutoUpdateService.class);
+        startService(intent);
     }
 
     private void loadBingPic() {
